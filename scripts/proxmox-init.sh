fb2b163bfc555b64ad4cb9ba9a91fe7497b15fd0
zenodotus280 <16273022+zenodotus280@users.noreply.github.com>
2024-12-07 14:46:09 -0800

    add: basic Proxmox setup script

---
+++ scripts/proxmox-init.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Post-Install Cleanup
+bash -c "$(wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/misc/post-pve-install.sh)"
+
+# Kernel Cleanup
+bash -c "$(wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/misc/kernel-clean.sh)"
+
+# Install Microcode
+bash -c "$(wget -qLO - https://github.com/community-scripts/ProxmoxVE/raw/main/misc/microcode.sh)"
+
+# Install and Configure Syncthing
+apt install extrepo
+extrepo enable syncthing
+apt update
+apt install syncthing
+
+adduser syncthing
+systemctl enable syncthing@syncthing.service --now
+systemctl status syncthing@syncthing.service | grep GUI
+sed -i -e 's/<address>127\.0\.0\.1:8384<\/address>/<address>0.0.0.0:8384<\/address>/' /home/syncthing/.local/state/syncthing/config.xml
+systemctl restart syncthing@syncthing.service --now
+
+# Install Netdata
+wget -O /tmp/netdata-kickstart.sh https://my-netdata.io/kickstart.sh && sh /tmp/netdata-kickstart.sh --stable-channel --disable-telemetry
+
+# Install Tailscale
+curl -fsSL https://tailscale.com/install.sh | sh
